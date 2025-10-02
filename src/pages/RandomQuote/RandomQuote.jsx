b9ee0fe6af9af7365189d36b871b02c1c80ea5f1
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-10-02 19:00:22 +0300

    Integrated a public API, using axios, for getting a random quote and display it

--- src/pages/RandomQuote/RandomQuote.jsx
+++ src/pages/RandomQuote/RandomQuote.jsx
@@ -1,8 +1,27 @@
 import { MdFavoriteBorder, MdOutlineFavorite } from 'react-icons/md';
 
+import axios from 'axios';
+
 import styles from './RandomQuote.module.css';
+import { useState } from 'react';
 
 const RandomQuote = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const getRandomQuote = async () => {
+    try {
+      const response = await axios.get(
+        'https://quoteslate.vercel.app/api/quotes/random'
+      );
+      const { quote, author } = response.data;
+      setQuote(quote);
+      setAuthor(author);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -10,12 +29,16 @@ const RandomQuote = () => {
         <div className={styles.quoteContainer}>
           <div className={styles.quote}>
             <p className={styles.quoteText}>
-              Click the button to get a random quote!
+              {quote ? quote : 'Click the button to get a random quote!'}
             </p>
-            <p className={styles.quoteAuthor}>© Mikhail</p>
+            <p className={styles.quoteAuthor}>
+              © {author ? author : 'from admine with love'}
+            </p>
           </div>
           <div className={styles.buttons}>
-            <button className={styles.generateBtn}>Get random quote</button>
+            <button className={styles.generateBtn} onClick={getRandomQuote}>
+              Get random quote
+            </button>
             <button className={styles.favoriteBtn}>
               <MdFavoriteBorder />
             </button>
