6b3dd8c246652a6fc22bf74acd110e9af6412498
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-10-13 10:48:37 +0300

    Setting Navbar component adaptivity

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,49 @@
+import { HiOutlineMenu } from 'react-icons/hi';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <>
-      <div className="navbar-header">
-        <p className="navbar-logo">ShevchenkoM</p>
-        <ul className="navbar-menu">
-          <NavLink
-            to={'random-quotes'}
-            className={({ isActive }) =>
-              isActive ? 'nav-link-active' : 'nav-link'
-            }
-          >
-            Random Quote
-          </NavLink>
-          <NavLink
-            to={'book-library'}
-            className={({ isActive }) =>
-              isActive ? 'nav-link-active' : 'nav-link'
-            }
-          >
-            Book Library
-          </NavLink>
-          <NavLink
-            to={'/'}
-            className={({ isActive }) =>
-              isActive ? 'nav-link-active' : 'nav-link'
-            }
-          >
-            About me
-          </NavLink>
-        </ul>
+      <div className="header">
+        <p className="header-logo">ShevchenkoM</p>
+        <nav className={`header-nav ${openMenu ? 'active' : ''}`}>
+          <ul className="header-nav-list">
+            <NavLink
+              to={'random-quotes'}
+              className={({ isActive }) =>
+                isActive ? 'header-nav-link active' : 'header-nav-link'
+              }
+            >
+              Random Quote
+            </NavLink>
+            <NavLink
+              to={'book-library'}
+              className={({ isActive }) =>
+                isActive ? 'header-nav-link active' : 'header-nav-link'
+              }
+            >
+              Book Library
+            </NavLink>
+            <NavLink
+              to={'/'}
+              className={({ isActive }) =>
+                isActive ? 'header-nav-link active' : 'header-nav-link'
+              }
+            >
+              About me
+            </NavLink>
+          </ul>
+        </nav>
+        <button
+          className="navbar-menu-btn"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          <HiOutlineMenu />
+        </button>
       </div>
     </>
   );
